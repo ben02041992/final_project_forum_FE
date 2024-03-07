cccd432cf842501f3bf08ab2dc4681426e8a9d3c
quinn <williammsullivansabre@gmail.com>
2024-03-07 15:19:23 +0000

    FE: gamepage and modal

---
+++ src/components/modal/Messagemodal.jsx
@@ -0,0 +1,47 @@
+import React from "react";
+import "./Messagemodal.css";
+
+const Messagemodal = ({ game, onClose }) => {
+  const handleContentClick = (e) => {
+    // Prevent event propagation to the overlay, which triggers the modal closure
+    e.stopPropagation();
+  };
+
+  return (
+    <div className="modal-overlay" onClick={onClose}>
+      <div className="modal-card" onClick={handleContentClick}>
+        <div className="modal-header">
+          <h2>{game.name}</h2>
+          <button onClick={onClose}>Close</button>
+        </div>
+        <div className="modal-content">
+          <p>Released: {game.released}</p>
+          <p>Rating: {game.rating}</p>
+          <p>Genres: {game.genres.map((genre) => genre.name).join(", ")}</p>
+          <p>
+            Platforms:{" "}
+            {game.platforms
+              .map((platform) => platform.platform.name)
+              .join(", ")}
+          </p>
+          <p>{game.publishers}</p>
+        </div>
+        <div className="modal-messages">
+          <p>
+            Messages go here <br /> User: Message
+          </p>
+        </div>
+        <div className="messenger">
+          <input
+            className="input"
+            type="text"
+            placeholder="Write message here"
+          />
+          <button className="send-but">Send</button>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Messagemodal;
