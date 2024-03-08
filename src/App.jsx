44e2c660e44914d565f352097af8103122b6c531
rocoriyan <rcorrigan.other+github@gmail.com>
2024-03-08 15:54:43 +0000

    move nav to top of app jsx as otherwise it appears broken

--- src/App.jsx
+++ src/App.jsx
@@ -14,15 +14,15 @@ function App() {
 
   return (
     <>
+      <Navbar user={{ /* this will be on the main page i just put it here for testing */
+        username:"Username",
+        pfp:"./images/tempPFP.png"
+      }}/>
       {loginSubmitToggle ? (
         <Login onToggle={toggleComponent} />
       ) : (
         <Signup onToggle={toggleComponent} />
       )}
-      <Navbar user={{ /* this will be on the main page i just put it here for testing */
-        username:"Username",
-        pfp:"./images/tempPFP.png"
-      }}/>
       <Gamepage />
     </>
   );
